e2b7b5b6cdfc50fa1269409d091e0f09dd66ec31
phamhieu215 <pvhncnh@gmail.com>
2026-05-14 14:16:12 +0700

    init commit

--- src/test/java/AutomationTest/TestComponents/BaseTest.java
+++ src/test/java/AutomationTest/TestComponents/BaseTest.java
@@ -11,10 +11,12 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -32,12 +34,19 @@ public class BaseTest {
 		Properties prop = new Properties();
 		FileInputStream fis =new FileInputStream(System.getProperty("user.dir")+"//src//main//java//AutomationTest//resources//GlobalData.properties");
 		prop.load(fis);
-		String browserName = prop.getProperty("browser");
+		String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") :prop.getProperty("browser");
+		//String browserName = prop.getProperty("browser");
 		
-		if(browserName.equalsIgnoreCase("chrome")) {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		
+		if(browserName.contains("chrome")) {
+		    ChromeOptions options = new ChromeOptions();
+		    WebDriverManager.chromedriver().setup();
+
+		    if(browserName.contains("headless")) {
+		        options.addArguments("headless");
+		    }
+
+		    driver = new ChromeDriver(options);
+		    driver.manage().window().setSize(new Dimension(1440,900));
 		}
 		else if(browserName.equalsIgnoreCase("firefox"))
 		{
